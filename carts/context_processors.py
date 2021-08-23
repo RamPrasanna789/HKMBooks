8bf789aaf796903bf33561767a0d2f6988fc1c11
RamPrasanna789 <rampkrishna789@gmail.com>
2021-08-23 17:59:30 +0530

    this is my sixth commit

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,4 +1,3 @@
-from django.shortcuts import redirect
 from .models import CartItem, Cart
 from .views import _cart_id
 
@@ -9,7 +8,10 @@ def counter(request):
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
         except Cart.DoesNotExist:
