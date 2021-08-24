56200936bd7e5fe8287d22ec0694f57f7ddbf70e
RamPrasanna789 <rampkrishna789@gmail.com>
2021-08-24 20:55:25 +0530

    help commit

--- orders/urls.py
+++ orders/urls.py
@@ -13,18 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
-from hkmbooks import settings, views
-from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('store/', include('store.urls')),
-    path('cart/', include('carts.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('orders/', include('orders.urls')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('place_order/', views.place_order, name='place_order'),
+    path('payments/', views.payments, name='payments'),
+]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from orders.models import Order
+from carts.models import Cart, CartItem
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from .forms import OrderForm
+import datetime
 
 # Create your views here.
+
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+def place_order(request):
+    current_user = request.user
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+
+    if cart_count <= 0:
+        return redirect('store')
+
+    grand_total = 0
+    shipping_charge = 50
+    total = 0
+    quantity = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    shipping_charge = 50
+    grand_total = total + shipping_charge
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.city = form.cleaned_data['city']
+            data.state = form.cleaned_data['state']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.shipping_charges = shipping_charge
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d") #20210305
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+            order = Order.objects.get(user=request.user, is_ordered=False, order_number=order_number)
+            return render(request, 'orders/payments.html', {'order': order, 'cart_items': cart_items, 'total': total, 'gtotal': grand_total, 'shipping': shipping_charge, 'stotal': grand_total * 100})
+        else:
+            return redirect('checkout')
