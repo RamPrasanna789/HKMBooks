80942104d25496ee84d8a6d1ba6d33d819e19bfe
RamPrasanna789 <rampkrishna789@gmail.com>
2021-08-26 14:15:27 +0530

    HKM BOOKS

--- store/views.py
+++ store/views.py
@@ -38,11 +38,14 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
-    try:
-        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
-    except OrderProduct.DoesNotExist:
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
         orderproduct = None
-
+        
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
